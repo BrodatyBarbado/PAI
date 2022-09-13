9caab9277b5595d1f5834beedaf3ce9423664ea8
BrodatyBarbado <m.smieszniak3205@wp.pl>
2022-09-13 10:44:58 +0200

    lekcja1

---
+++ lekcja1/hello.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Hello World</title>
+</head>
+<body>
+
+	<?php
+
+		$mycounter = 1;
+		echo "Hello world"."<br>".$mycounter."<br>"."Hello Friend";
+		
+	?>
+
+
+</body>
+</html>
