d431b12a656915104e0f3259390c324a111a6b71
Michał Śmieszniak (Uczeń) <msmieszniak190@technikumkreatywne.pl>
2022-09-16 15:40:41 +0200

    petle

---
+++ lekcja3/pętle.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>petla</title>
+</head>
+<body>
+
+	<?php
+	
+	/*for($count =1; $count <= 10; ++$count)
+	{	
+		echo "<br>";
+		for($count1 = 1; $count1 <= 10; ++$count1)
+		{
+			echo "$count * ". $count1 . " = " . $count * $count1 ;
+			echo "<br>";
+		}
+		
+	}
+	*/
+	
+        for($count =1; $count <= 6; ++$count)
+	{	
+		echo "<br>";
+		for($count1 = 1; $count1 <= 6; ++$count1)
+		{
+			echo "*" ;
+			
+		}
+		
+     }   
+	?>
+
+
+</body>
+</html>
